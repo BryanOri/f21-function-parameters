690c4db8936d583dcf8d9ea9f6a88fa0f351fc89
BryanOri <bryanorihuela@computingforall.org>
2021-09-23 17:56:29 -0800

    stuff

--- function-parameters.js
+++ function-parameters.js
@@ -3,8 +3,15 @@
 //      will receive numbers from outside the function.
 let x = 5;
 let y = 2;
+function calc(x, y){
 let product = x * y;
 console.log(x + " * " + y + " = " + product);
+};
+
+calc();
+calc(10,12);
+calc(5,8);
+calc(3,5);
 
 // 3) Call your new function below 3 separate times with
 //      different supplied x's and y's
